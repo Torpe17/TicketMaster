397e4586b9d64737178e048628d04bb0685d81c4
TheHunSaiyan <panczelbarnusdarth@gmail.com>
2025-04-01 14:07:49 +0200

    IGenericRepository created

---
+++ TicketMaster.DataContext/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketMaster.DataContext.Repositories
+{
+    interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAsync(string[]? includedProperties = null);
+        Task<TEntity?> GetByIdAsync(int id, string[]? includedReferences = null, string[]? includedCollections = null);
+        Task InsertAsync(TEntity entity);
+        void Update(TEntity entity);
+        Task DeleteByIdAsync(int id);
+    }
+}
