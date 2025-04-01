4ab8b163272b5748f24c4ad7ead40243451136f8
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-04-01 14:30:54 +0200

    add UnitOfWork logic
    - removed IUnitOfWork
    - added generic repo instantiation

--- TicketMaster.DataContext/UnitsOfWork/UnitOfWork.cs
+++ TicketMaster.DataContext/UnitsOfWork/UnitOfWork.cs
@@ -1,24 +1,29 @@
 using TicketMaster.DataContext.Context;
+using TicketMaster.DataContext.Models;
+using TicketMaster.DataContext.Repositories;
 
 namespace TicketMaster.DataContext.UnitsOfWork;
 
-public class UnitOfWork : IUnitOfWork
+public class UnitOfWork
 {
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+        FilmRepository = new GenericRepository<Film>(context);
+        PurchaseRepository = new GenericRepository<Purchase>(context);
+        RoomRepository = new GenericRepository<Room>(context);
+        ScreeningRepository = new GenericRepository<Screening>(context);
+        TicketRepository = new GenericRepository<Ticket>(context);
+        UserRepository = new GenericRepository<User>(context);
+    }
     private readonly AppDbContext _context;
-    // Uncomment when implemented
-    // IFilmRepository _filmRepository;
-    // IPurchaseRepository _purchaseRepository;
-    // IRoomRepository _roomRepository;
-    // IScreeningRepository _screeningRepository;
-    // ITicketRepository _ticketRepository;
-    // IUserRepository _userRepository;
-    public void Dispose()
-    {
-        // TODO release managed resources here
-    }
-
-    public void Save()
-    {
-        throw new NotImplementedException();
-    }
+    public GenericRepository<Film> FilmRepository { get; set; }
+    public GenericRepository<Purchase> PurchaseRepository { get; set; }
+    public GenericRepository<Room> RoomRepository { get; set;  }
+    public GenericRepository<Screening> ScreeningRepository { get; set; }
+    public GenericRepository<Ticket> TicketRepository { get; set; }
+    public GenericRepository<User> UserRepository { get; set; }
+    public void Dispose() { _context.Dispose(); }
+    public void Save() { _context.SaveChanges(); }
+    public async Task SaveAsync() { await _context.SaveChangesAsync(); }
 }
