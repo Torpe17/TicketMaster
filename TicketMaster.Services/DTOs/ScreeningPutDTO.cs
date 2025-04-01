85a585c5383b25bbb1cbee1662ad17754ecc0d34
TheHunSaiyan <panczelbarnusdarth@gmail.com>
2025-04-01 16:04:13 +0200

    Screening controller imlemented

---
+++ TicketMaster.Services/DTOs/ScreeningPutDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketMaster.DataContext.Models;
+
+namespace TicketMaster.Services.DTOs
+{
+    public class ScreeningPutDTO
+    {
+        public int? FilmId { get; set; }
+        public int? RoomId { get; set; }
+        public DateTime? Date { get; set; }
+        public Film? Film { get; set; }
+        public List<Ticket>? Tickets { get; set; }
+    }
+}
---
+++ TicketMaster/Controllers/ScreeningsController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Humanizer;
+using Humanizer.Localisation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TicketMaster.DataContext.Context;
+using TicketMaster.DataContext.Models;
+using TicketMaster.DataContext.UnitsOfWork;
+using TicketMaster.Services.DTOs;
+
+namespace TicketMaster.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScreeningsController : ControllerBase
+    {
+        private UnitOfWork unitOfWork;
+        private IMapper mapper;
+
+        public ScreeningsController(UnitOfWork _unitOfWork, IMapper _mapper)
+        {
+            unitOfWork = _unitOfWork;
+            mapper = _mapper;
+        }
+
+        // GET: api/Screenings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ScreeningGetDTO>>> GetScreenings()
+        {
+            var screenings = await unitOfWork.ScreeningRepository.GetAsync(
+                //includedProperties: ["Film", "Tickets"]
+                );
+            return mapper.Map<List<ScreeningGetDTO>>(screenings);
+        }
+
+        // GET: api/Screenings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ScreeningGetDTO>> GetScreening(int id)
+        {
+            var screening = await unitOfWork.ScreeningRepository.GetByIdAsync(
+                 id,
+                 includedReferences: ["Film"],
+                 includedCollections: ["Tickets"]
+                 );
+
+            if (screening == null)
+            {
+                return NotFound();
+            }
+            return mapper.Map<ScreeningGetDTO>(screening);
+        }
+
+        // PUT: api/Screenings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutScreening(int id, ScreeningPutDTO dto)
+        {
+            Screening? screening = await unitOfWork.ScreeningRepository.GetByIdAsync(id);
+
+            if (screening == null)
+            {
+                return NotFound();
+            }
+
+            var tickets = await unitOfWork.TicketRepository.GetAsync(
+                includedProperties: ["Screening"]
+                );
+
+            //_context.Entry(@event).State = EntityState.Modified;
+            //_unitOfWork.EventRepository.Update(@event);
+            mapper.Map(dto, screening);
+
+            try
+            {
+                //await _context.SaveChangesAsync();
+                await unitOfWork.SaveAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await ScreeningExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok();
+        }
+
+        // POST: api/Screenings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult> PostScreening(ScreeningPostDTO screening)
+        {
+            /*if (screening.RoomId != null)
+            {
+                Room? room = await unitOfWork.RoomRepository.GetByIdAsync(screening.RoomId);
+                if (room == null)
+                {
+                    return BadRequest("Room does not exist");
+                }
+            }
+            if (screening.FilmId != null)
+            {
+                Film? film = await unitOfWork.FilmRepository.GetByIdAsync(screening.FilmId);
+                if (film == null)
+                {
+                    return BadRequest("Film does not exist");
+                }
+            }*/
+
+            Screening newScreening = mapper.Map<Screening>(screening);
+
+            await unitOfWork.ScreeningRepository.InsertAsync(newScreening);
+            await unitOfWork.SaveAsync();
+
+            return Created();
+        }
+
+        // DELETE: api/Screenings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteScreening(int id)
+        {
+            await unitOfWork.ScreeningRepository.DeleteByIdAsync(id);
+            await unitOfWork.SaveAsync();
+
+            return NoContent();
+        }
+
+        private async Task<bool> ScreeningExists(int id)
+        {
+            return await unitOfWork.ScreeningRepository.GetByIdAsync(id) != null;
+        }
+    }
+}
