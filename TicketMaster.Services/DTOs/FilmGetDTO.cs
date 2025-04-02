f9be1fd816664a150c369d1b121389dd1a60e311
TheAshenWitch <jaholamark410@gmail.com>
2025-04-02 16:53:18 +0200

    Created Film DTOs, created FilmController, created FilmProfile (mapping). Modified the entity "Film" -> empty screening list is created when film is created

---
+++ TicketMaster.Services/DTOs/FilmGetDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketMaster.DataContext.Models;
+
+namespace TicketMaster.Services.DTOs
+{
+    public class FilmGetDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public int Length { get; set; }
+        public string Description { get; set; }
+        public int? AgeRating { get; set; }
+        //public List<Screening>? Screenings { get; set; }  //TODO!!! : Change to ScreeningGetDTO
+    }
+}
