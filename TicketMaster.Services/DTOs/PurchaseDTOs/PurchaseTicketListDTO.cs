0ce77ba92549dfe61d4385e7850505ad2fe6206c
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-04-02 20:39:43 +0200

    add PurchaseTicketListDTO fields

--- TicketMaster.Services/DTOs/PurchaseDTOs/PurchaseTicketListDTO.cs
+++ TicketMaster.Services/DTOs/PurchaseDTOs/PurchaseTicketListDTO.cs
@@ -2,5 +2,9 @@ namespace TicketMaster.Services.DTOs.PurchaseDTOs;
 
 public class PurchaseTicketListDTO
 {
-    
+    public int Id { get; set; }
+    public int? PurchaseId { get; set; }
+    public int SeatRow { get; set; }
+    public int SeatColumn { get; set; }
+    public double Price { get; set; }
 }
