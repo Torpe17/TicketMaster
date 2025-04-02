d69b2a30688937e1376c245f7678ba980b81a4de
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-04-02 17:03:42 +0200

    Update PurchasePutDto.cs
    added fields

--- TicketMaster.Services/DTOs/PurchasePutDto.cs
+++ TicketMaster.Services/DTOs/PurchasePutDto.cs
@@ -1,6 +1,11 @@
+using TicketMaster.DataContext.Models;
+
 namespace TicketMaster.Services.DTOs;
 
 public class PurchasePutDto
 {
-    
+    public bool SetUserId { get; set; }
+    public int? UserId { get; set; }
+    public DateTime? PurchaseDate { get; set; }
+    public double? TotalPrice { get; set; }
 }
