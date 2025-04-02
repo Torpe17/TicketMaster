efffb4ec44f0083e4547f9e682e1ead2d0c17697
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-04-02 21:27:52 +0200

    Update PurchasePostDTO.cs
    added fields to the class

--- TicketMaster.Services/DTOs/PurchaseDTOs/PurchasePostDTO.cs
+++ TicketMaster.Services/DTOs/PurchaseDTOs/PurchasePostDTO.cs
@@ -2,5 +2,8 @@ namespace TicketMaster.Services.DTOs.PurchaseDTOs;
 
 public class PurchasePostDTO
 {
-    
+    public int Id { get; set; }
+    public int? UserId { get; set; }
+    public DateTime PurchaseDate { get; set; }
+    public double TotalPrice { get; set; }
 }
