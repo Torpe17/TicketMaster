16ac5dc06f55b4ff90283f2f2f8731fa44a62867
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-04-02 15:15:10 +0200

    Update PurchaseGetDto.cs
    added correct fields

--- TicketMaster.Services/DTOs/PurchaseGetDto.cs
+++ TicketMaster.Services/DTOs/PurchaseGetDto.cs
@@ -2,5 +2,10 @@ namespace TicketMaster.Services.DTOs;
 
 public class PurchaseGetDto
 {
-    
+    public int Id { get; set; }
+    public int? UserId { get; set; }
+    public string? UserName { get; set; }
+    public double TotalPrice { get; set; }
+    public int? TicketCount { get; set; }
+    public DateTime PurchaseDate { get; set; }
 }
