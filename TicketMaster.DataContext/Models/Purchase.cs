db3daa10d2a0b11d2b11672b8e0493bafc7402df
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-04-02 21:27:42 +0200

    Update Purchase.cs
    corrected the user object being nullable

--- TicketMaster.DataContext/Models/Purchase.cs
+++ TicketMaster.DataContext/Models/Purchase.cs
@@ -3,10 +3,10 @@
     public class Purchase
     {
         public int Id { get; set; }
-        public int? UserId { get; set; }
         public DateTime PurchaseDate { get; set; }
         public double TotalPrice { get; set; }
-        public User User { get; set; }
+        public int? UserId { get; set; }
+        public User? User { get; set; }
         public List<Ticket> Tickets { get; set; } = new List<Ticket>();
     }
 }
