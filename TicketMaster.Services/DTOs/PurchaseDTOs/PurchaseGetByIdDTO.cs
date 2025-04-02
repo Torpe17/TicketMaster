cd7d87de5388bb1c4e663e2d59990531c71f1850
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-04-02 21:25:59 +0200

    Update PurchaseGetByIdDTO.cs
    added fields to the class

--- TicketMaster.Services/DTOs/PurchaseDTOs/PurchaseGetByIdDTO.cs
+++ TicketMaster.Services/DTOs/PurchaseDTOs/PurchaseGetByIdDTO.cs
@@ -2,5 +2,10 @@ namespace TicketMaster.Services.DTOs.PurchaseDTOs;
 
 public class PurchaseGetByIdDTO
 {
-    
+    public int Id { get; set; }
+    public int? UserId { get; set; }
+    public string? UserName { get; set; }
+    public double TotalPrice { get; set; }
+    public DateTime PurchaseDate { get; set; }
+    public List<PurchaseTicketListDTO> Tickets { get; set; }
 }
