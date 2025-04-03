a78976ad8ebfbf51cb50a0a50d5470b03f724ce2
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-04-03 09:28:18 +0200

    Update PurchaseController.cs
    defined routes explicitly
    added error handling
    added logger use
    expanded swagger documentation on purchase routes

--- TicketMaster/Controllers/PurchaseController.cs
+++ TicketMaster/Controllers/PurchaseController.cs
@@ -10,10 +10,10 @@ using TicketMaster.Services.DTOs.PurchaseDTOs;
 namespace TicketMaster.Controllers;
 
 [ApiController]
-[Route("api/[controller]/[action]")]
+[Route("api/purchase")]
 public class PurchaseController : Controller
 {
-    private ILogger<PurchaseController> _logger;
+    private readonly ILogger<PurchaseController> _logger;
     private UnitOfWork _unitOfWork;
     private IMapper _mapper;
 
@@ -24,83 +24,152 @@ public class PurchaseController : Controller
         _logger = logger;
     }
     
-    [HttpGet]
+    [HttpGet("getall")]
+    [ProducesResponseType(typeof(IEnumerable<PurchaseGetDTO>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<ActionResult<IEnumerable<PurchaseGetDTO>>> GetPurchases()
     {
-        var purchases = await _unitOfWork.PurchaseRepository.GetAsync(
-            includedProperties: ["User", "Tickets"]
+        try
+        {
+            var purchases = await _unitOfWork.PurchaseRepository.GetAsync(
+                includedProperties: ["User", "Tickets"]
             );
-        return _mapper.Map<List<PurchaseGetDTO>>(purchases);
+            if (purchases == null || !purchases.Any()) { return Ok(Enumerable.Empty<PurchaseGetDTO>()); }
+            
+            return Ok(_mapper.Map<List<PurchaseGetDTO>>(purchases));
+        }
+        catch (DbException db)
+        {
+            _logger.LogError(db, "Unexpected error occurred in the database while getting all Purchases.");
+            return StatusCode(500, "A database error occured.");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Unexpected error occurred while getting all Purchases.");
+            return StatusCode(500, "An unexpected error occured.");
+            
+        }
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("get/{id}")]
+    [ProducesResponseType(typeof(PurchaseGetDTO), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<PurchaseGetByIdDTO>> GetPurchase(int id)
     {
+        if (id <= 0) return BadRequest("ID must be greater than zero.");
         // var purchase = await _unitOfWork.PurchaseRepository.GetByIdAsync(id, includedReferences: ["User"], includedCollections: ["Tickets"]); // race condition if both are set at the same time
-        var purchase = await _unitOfWork.PurchaseRepository.GetByIdAsync(id);
-        await _unitOfWork.PurchaseRepository.GetByIdAsync(id, includedReferences: ["User"]);
-        await _unitOfWork.PurchaseRepository.GetByIdAsync(id, includedCollections: ["Tickets"]);
-        
-        if (purchase == null)
+        try
         {
-            return NotFound();
+            var purchase = await _unitOfWork.PurchaseRepository.GetByIdAsync(id);
+            await _unitOfWork.PurchaseRepository.GetByIdAsync(id, includedReferences: ["User"]);
+            await _unitOfWork.PurchaseRepository.GetByIdAsync(id, includedCollections: ["Tickets"]);
+
+            if (purchase == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<PurchaseGetByIdDTO>(purchase));
         }
-        return Ok(_mapper.Map<PurchaseGetByIdDTO>(purchase));
+        catch (DbException db)
+        {
+            _logger.LogError(db, $"Unexpected error occurred in the database while getting Purchase by ID: {id}.");
+            return StatusCode(500, "A database error occured.");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, $"Unexpected error occurred while getting Purchase by ID: {id}.");
+            return StatusCode(500, "An unexpected error occured.");
+        }
+        
     }
 
-    [HttpPost]
+    [HttpPost("create")]
+    [ProducesResponseType(typeof(PurchaseGetDTO), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> PostPurchase(PurchasePostDTO @purchase)
     {
-        // TO-DO: error handling
-        if (@purchase.UserId != null)
+        try
         {
-            User? user = await _unitOfWork.UserRepository.GetByIdAsync(@purchase.UserId.Value);
-            if (user == null)
+            if (@purchase.UserId != null)
             {
-                return BadRequest("User does not exist.");
+                User? user = await _unitOfWork.UserRepository.GetByIdAsync(@purchase.UserId.Value);
+                if (user == null) { return BadRequest("User does not exist."); }
             }
-        }
-        
-        Purchase newPurchase = _mapper.Map<Purchase>(@purchase);
-        
-        await _unitOfWork.PurchaseRepository.InsertAsync(newPurchase);
-        await _unitOfWork.SaveAsync();
 
-        return Created();
+            Purchase newPurchase = _mapper.Map<Purchase>(@purchase);
+
+            await _unitOfWork.PurchaseRepository.InsertAsync(newPurchase);
+            await _unitOfWork.SaveAsync();
+
+            return Created();
+        }
+        catch (DbException db)
+        {
+            _logger.LogError(db, $"Unexpected error occurred in the database while creating Purchase.");
+            return StatusCode(500, "A database error occured.");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, $"Unexpected error occurred while creating Purchase.");
+            return StatusCode(500, "An unexpected error occured.");
+        }
     }
 
-    [HttpPut("{id}")]
+    [HttpPut("update/{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> PutPurchase(int id, PurchasePutDTO purchase)
     {
-        Purchase? p = await _unitOfWork.PurchaseRepository.GetByIdAsync(id);
-        if (p == null) return NotFound();
-        
-        _mapper.Map(purchase, p);
         try
         {
+            if (id <= 0) return BadRequest("ID must be greater than zero.");
+            Purchase? p = await _unitOfWork.PurchaseRepository.GetByIdAsync(id);
+            if (p == null) return NotFound("Purchase specified by ID not found.");
+
+            _mapper.Map(purchase, p);
             await _unitOfWork.SaveAsync();
+
+            return NoContent();
+        }
+        catch (DbException db)
+        {
+            _logger.LogError(db, $"Unexpected error occurred in the database while updating Purchase with ID: {id}.");
+            return StatusCode(500, "A database error occured.");
         }
         catch (Exception e)
         {
-            // TODO: better error handling
-            return NotFound(e.Message);
+            _logger.LogError(e, $"Unexpected error occurred while updating Purchase with ID: {id}.");
+            return StatusCode(500, "An unexpected error occured.");
         }
-
-        return Ok();
     }
     
-    [HttpDelete]
-    public IActionResult Delete([FromBody] int id)
+    [HttpDelete("delete/{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Delete(int id)
     {
+        if(id <= 0) return BadRequest("ID must be greater than zero.");
         try
         {
-            var result = _unitOfWork.PurchaseRepository.DeleteByIdAsync(id);
-            _unitOfWork.Save();
-            return Ok(result);
+            await _unitOfWork.PurchaseRepository.DeleteByIdAsync(id);
+            await _unitOfWork.SaveAsync();
+
+            return NoContent();
+        }
+        catch (DbException db)
+        {
+            _logger.LogError(db, $"Unexpected error occurred in the database while deleting Purchase with ID: {id}.");
+            return StatusCode(500, "A database error occured.");
         }
         catch (Exception e)
         {
-            return BadRequest(e.Message);
+            _logger.LogError(e, $"Unexpected error occurred while deleting Purchase with ID: {id}.");
+            return StatusCode(500, "An unexpected error occured.");
         }
     }
 }
