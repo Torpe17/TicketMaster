fb4fcae870644eba9751de5b0122823867614095
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-04-03 10:20:38 +0200

    Update Room.cs
    added additional fields

--- TicketMaster.DataContext/Models/Room.cs
+++ TicketMaster.DataContext/Models/Room.cs
@@ -4,7 +4,18 @@ namespace TicketMaster.DataContext.Models;
     {
         public int RoomId { get; set; }
         public String Name { get; set; }
-        public int MaxSeatRow { get; set; }
-        public int MaxSeatColumn { get; set; }
+        public string? Description { get; set; }
+        public RoomType Type { get; set; } = RoomType.Other;
+        public int? MaxSeatRow { get; set; }
+        public int? MaxSeatColumn { get; set; }
+        public int Capacity { get; set; }
         public List<Screening> Screenings { get; set; }
+
+        public enum RoomType
+        {
+            Movie,
+            Theater,
+            Concert,
+            Other
+        }
     }
