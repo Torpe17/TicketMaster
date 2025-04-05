70f137bebefbc3403ca614662285bd1d3433aef8
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-04-05 23:05:00 +0200

    refactor room fields
    created roomtype class,
    added constructedat datetime field to room

--- TicketMaster.DataContext/Models/Room.cs
+++ TicketMaster.DataContext/Models/Room.cs
@@ -5,19 +5,13 @@ namespace TicketMaster.DataContext.Models;
         public int RoomId { get; set; }
         public String Name { get; set; }
         public string? Description { get; set; }
-        public RoomType Type { get; set; } = RoomType.Other;
+        public int? RoomTypeId { get; set; }
+        public RoomType? RoomType { get; set; }
         public int? MaxSeatRow { get; set; }
         public int? MaxSeatColumn { get; set; }
         public int Capacity { get; set; }
         public bool DisabilityFriendly { get; set; } = false;
         public int? ComfortLevel { get; set; }
-        public List<Screening> Screenings { get; set; }
-
-        public enum RoomType
-        {
-            Movie,
-            Theater,
-            Concert,
-            Other
-        }
+        public DateTime? ConstructedAt { get; set; }
+        public List<Screening> Screenings { get; set; } = new List<Screening>();
     }
