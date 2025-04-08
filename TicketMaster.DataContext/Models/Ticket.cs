77217e9af677ebfbea2218456598fbc9784bcc47
TheHunSaiyan <panczelbarnusdarth@gmail.com>
2025-04-08 08:31:53 +0200

    SeatRow and SeatColumn now nullable to match room

--- TicketMaster.DataContext/Models/Ticket.cs
+++ TicketMaster.DataContext/Models/Ticket.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public int ScreeningId { get; set; }
         public int? PurchaseId { get; set; }
-        public int SeatRow { get; set; }
-        public int SeatColumn { get; set; }
+        public int? SeatRow { get; set; }
+        public int? SeatColumn { get; set; }
         public double Price { get; set; }
         public Purchase? Purchase { get; set; }
         public Screening Screening { get; set; }
