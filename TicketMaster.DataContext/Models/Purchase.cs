fc11e64a2fa3a6053be9be03492b0d98124a7c38
Gurumlai Gergely <gergely.gurumlai03@gmail.com>
2025-04-26 23:24:18 +0200

    purchaseService wip

--- TicketMaster.DataContext/Models/Purchase.cs
+++ TicketMaster.DataContext/Models/Purchase.cs
@@ -2,11 +2,13 @@
 {
     public class Purchase
     {
+        [Key]
         public int Id { get; set; }
-        public DateTime PurchaseDate { get; set; }
-        public double TotalPrice { get; set; }
         public int? UserId { get; set; }
         public User? User { get; set; }
         public List<Ticket> Tickets { get; set; } = new List<Ticket>();
+        public double TotalPrice => Tickets.Sum(t => t.Price);
+        public DateTime PurchaseDate { get; set; }
+
     }
 }
